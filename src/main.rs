23e0057999297591cedb0172c73991186e9b2240
Dietrich Epp <depp@zdome.net>
2020-07-22 15:27:57 -0400

    Use OpenGL 3.3 loader based on gl_generator

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ use glutin::ContextBuilder;
 use std::ffi::{c_void, CStr, CString};
 use takeable_option::Takeable;
 
-use gles30::*;
+use gl;
+use gl::types::*;
 
 pub fn main() {
     let (raw_context, el) = {
@@ -27,14 +28,11 @@ pub fn main() {
         raw_context.get_pixel_format()
     );
 
-    let gl = GlFns::load_with(|c_char_ptr| {
-        let cstr = unsafe { CStr::from_ptr(c_char_ptr) };
-        raw_context.get_proc_address(cstr.to_str().unwrap()) as *mut std::ffi::c_void
-    });
-    assert!(gl.ClearColor_is_loaded());
-    assert!(gl.Clear_is_loaded());
+    gl::load_with(|name| raw_context.get_proc_address(name));
+    assert!(gl::ClearColor::is_loaded());
+    assert!(gl::Clear::is_loaded());
 
-    let program = init_shaders(&gl);
+    let program = init_shaders();
     println!("program: {}", program);
     let mut degree = 0.0;
     let mut x: i32 = 2200;
@@ -56,7 +54,7 @@ pub fn main() {
                 WindowEvent::Resized(physical_size) => {
                     raw_context.resize(physical_size);
                     unsafe {
-                        gl.Viewport(
+                        gl::Viewport(
                             0,
                             0,
                             physical_size.width as i32,
@@ -72,7 +70,7 @@ pub fn main() {
             },
             Event::RedrawRequested(_) => {
                 println!("el {:?}", event);
-                render(&gl, program, x, y);
+                render(program, x, y);
                 raw_context.swap_buffers().unwrap();
                 degree += 1.0f32;
             }
@@ -83,26 +81,26 @@ pub fn main() {
 
 const VERTEX_SRC: &str = include_str!("vertex_shader.glsl");
 const FRAGMENT_SRC: &str = include_str!("fragment_shader.glsl");
-const TRI_VERTS: [gles30::GLfloat; 6] = [40.0f32, 16.0f32, 104.0f32, 16.0f32, 104.0f32, 80.0f32];
-const TRI_COLS: [gles30::GLfloat; 12] = [
+const TRI_VERTS: [GLfloat; 6] = [40.0f32, 16.0f32, 104.0f32, 16.0f32, 104.0f32, 80.0f32];
+const TRI_COLS: [GLfloat; 12] = [
     0.0f32, 1.0f32, 0.0f32, 0.0f32, 0.0f32, 1.0f32, 0.0f32, 0.0f32, 1.0f32, 1.0f32, 0.0f32, 0.0f32,
 ];
 
-fn init_shaders(gl: &GlFns) -> gles30::GLuint {
+fn init_shaders() -> GLuint {
     unsafe {
-        let vshader = gl.CreateShader(gles30::GL_VERTEX_SHADER);
+        let vshader = gl::CreateShader(gl::VERTEX_SHADER);
         let vsrc_ptr: &CStr = &CString::new(VERTEX_SRC).unwrap();
-        gl.ShaderSource(vshader, 1, &vsrc_ptr.as_ptr(), std::ptr::null());
-        gl.CompileShader(vshader);
+        gl::ShaderSource(vshader, 1, &vsrc_ptr.as_ptr(), std::ptr::null());
+        gl::CompileShader(vshader);
         let mut compiled = 0;
-        gl.GetShaderiv(vshader, gles30::GL_COMPILE_STATUS, &mut compiled);
+        gl::GetShaderiv(vshader, gl::COMPILE_STATUS, &mut compiled);
         println!("vshader compilation: {}", compiled);
         if compiled == 0 {
             let mut info_len = 0;
-            gl.GetShaderiv(vshader, gles30::GL_INFO_LOG_LENGTH, &mut info_len);
+            gl::GetShaderiv(vshader, gl::INFO_LOG_LENGTH, &mut info_len);
             if info_len > 0 {
                 let mut buf: Vec<i8> = std::iter::repeat(0).take((info_len) as usize).collect();
-                gl.GetShaderInfoLog(vshader, info_len, std::ptr::null_mut(), buf.as_mut_ptr());
+                gl::GetShaderInfoLog(vshader, info_len, std::ptr::null_mut(), buf.as_mut_ptr());
                 let ubuf: Vec<u8> = buf
                     .iter()
                     .map(|&x| x as u8)
@@ -115,19 +113,19 @@ fn init_shaders(gl: &GlFns) -> gles30::GLuint {
             }
         }
 
-        let fshader = gl.CreateShader(gles30::GL_FRAGMENT_SHADER);
+        let fshader = gl::CreateShader(gl::FRAGMENT_SHADER);
         let fsrc_ptr: &CStr = &CString::new(FRAGMENT_SRC).unwrap();
-        gl.ShaderSource(fshader, 1, &fsrc_ptr.as_ptr(), std::ptr::null());
-        gl.CompileShader(fshader);
+        gl::ShaderSource(fshader, 1, &fsrc_ptr.as_ptr(), std::ptr::null());
+        gl::CompileShader(fshader);
         let mut compiled = 0;
-        gl.GetShaderiv(fshader, gles30::GL_COMPILE_STATUS, &mut compiled);
+        gl::GetShaderiv(fshader, gl::COMPILE_STATUS, &mut compiled);
         println!("fshader compilation: {}", compiled);
         if compiled == 0 {
             let mut info_len = 0;
-            gl.GetShaderiv(fshader, gles30::GL_INFO_LOG_LENGTH, &mut info_len);
+            gl::GetShaderiv(fshader, gl::INFO_LOG_LENGTH, &mut info_len);
             if info_len > 0 {
                 let mut buf: Vec<i8> = std::iter::repeat(0).take((info_len) as usize).collect();
-                gl.GetShaderInfoLog(fshader, info_len, std::ptr::null_mut(), buf.as_mut_ptr());
+                gl::GetShaderInfoLog(fshader, info_len, std::ptr::null_mut(), buf.as_mut_ptr());
                 let ubuf: Vec<u8> = buf
                     .iter()
                     .map(|&x| x as u8)
@@ -142,10 +140,10 @@ fn init_shaders(gl: &GlFns) -> gles30::GLuint {
         println!("shaders: {}, {}", vshader, fshader);
         // currently assuming this compiles correctly
 
-        let program = gl.CreateProgram();
-        gl.AttachShader(program, vshader);
-        gl.AttachShader(program, fshader);
-        gl.LinkProgram(program);
+        let program = gl::CreateProgram();
+        gl::AttachShader(program, vshader);
+        gl::AttachShader(program, fshader);
+        gl::LinkProgram(program);
         return program;
     }
 }
@@ -158,51 +156,51 @@ const BITSTRING: [[u32; 4]; 4] = [
     [0x1425e1fe; 4],
 ];
 
-fn render(gl: &GlFns, program: gles30::GLuint, x: i32, y: i32) {
+fn render(program: GLuint, x: i32, y: i32) {
     unsafe {
-        gl.ClearColor(0.2, 0.3, 0.3, 1.0);
-        gl.Clear(gles30::GL_COLOR_BUFFER_BIT);
-        gl.UseProgram(program);
+        gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+        gl::Clear(gl::COLOR_BUFFER_BIT);
+        gl::UseProgram(program);
         // again, assuming this works
 
-        let pos_handle: gles30::GLuint = gl
-            .GetAttribLocation(program, CString::new("VertexPosition").unwrap().as_ptr())
-            as gles30::GLuint;
+        let pos_handle: GLuint =
+            gl::GetAttribLocation(program, CString::new("VertexPosition").unwrap().as_ptr())
+                as GLuint;
 
         println!("handle: {}", pos_handle);
 
-        let dims = gl.GetUniformLocation(program, CString::new("Dimensions").unwrap().as_ptr());
+        let dims = gl::GetUniformLocation(program, CString::new("Dimensions").unwrap().as_ptr());
         println!("uniform: {}", dims);
-        gl.Uniform2f(dims, x as f32, y as f32);
+        gl::Uniform2f(dims, x as f32, y as f32);
 
-        gl.VertexAttribPointer(
+        gl::VertexAttribPointer(
             pos_handle,
             2,
-            gles30::GL_FLOAT,
-            gles30::GL_FALSE as u8,
+            gl::FLOAT,
+            gl::FALSE as u8,
             0,
             TRI_VERTS.as_ptr() as *const c_void,
         );
 
-        gl.EnableVertexAttribArray(pos_handle);
+        gl::EnableVertexAttribArray(pos_handle);
 
         let mut block = 0;
-        gl.GenBuffers(1, &mut block);
-        gl.BindBuffer(gles30::GL_UNIFORM_BUFFER, block);
-        gl.BufferData(
-            gles30::GL_UNIFORM_BUFFER,
+        gl::GenBuffers(1, &mut block);
+        gl::BindBuffer(gl::UNIFORM_BUFFER, block);
+        gl::BufferData(
+            gl::UNIFORM_BUFFER,
             16 * 4,
             BITSTRING.as_ptr() as *const c_void,
-            gles30::GL_STATIC_DRAW,
+            gl::STATIC_DRAW,
         );
         println!("buffer object: {}", block);
 
         let block_handle =
-            gl.GetUniformBlockIndex(program, CString::new("bitstring").unwrap().as_ptr());
+            gl::GetUniformBlockIndex(program, CString::new("bitstring").unwrap().as_ptr());
         println!("uniform buffer: {}", block_handle);
-        gl.UniformBlockBinding(program, block_handle, 2);
-        gl.BindBufferBase(GL_UNIFORM_BUFFER, 2, block);
+        gl::UniformBlockBinding(program, block_handle, 2);
+        gl::BindBufferBase(gl::UNIFORM_BUFFER, 2, block);
 
-        gl.DrawArraysInstanced(gles30::GL_TRIANGLES, 0, 3, 4 * 32);
+        gl::DrawArraysInstanced(gl::TRIANGLES, 0, 3, 4 * 32);
     }
 }
